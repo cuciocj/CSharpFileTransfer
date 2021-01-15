912263e4b03be9b144971744ff227e4722e1189b
Christopher John Cucio <cuciocj@gmail.com>
2021-01-15 17:33:58 +1300

    fixed settings apply event issue

--- CSharpFileTransferClient/Settings.cs
+++ CSharpFileTransferClient/Settings.cs
@@ -20,19 +20,21 @@ namespace CSharpFileTransferClient {
 
         public Settings() {
             InitializeComponent();
-            initializeDefaultSettings();
+            InitializeDefaultSettings();
         }
 
-        private void initializeDefaultSettings() {
+        private void InitializeDefaultSettings() {
             numMaxSimDownload.Value = Convert.ToDecimal(MAX_DOWNLOAD);
             numMaxSizeAllowed.Value = Convert.ToDecimal(MAX_SIZE_ALLOWED);
             txtDownloadsDirectory.Text = DOWNLOADS_DIRECTORY;
         }
 
         private void btnApplySettings_Click(object sender, EventArgs e) {
-            MAX_DOWNLOAD = Convert.ToInt32(numMaxSimDownload);
-            MAX_SIZE_ALLOWED = Convert.ToInt32(numMaxSizeAllowed);
+            MAX_DOWNLOAD = Convert.ToInt32(numMaxSimDownload.Value);
+            MAX_SIZE_ALLOWED = Convert.ToInt32(numMaxSizeAllowed.Value);
             DOWNLOADS_DIRECTORY = txtDownloadsDirectory.Text;
+
+            this.Close();
         }
     }
 }
